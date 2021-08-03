15e5ce41bbc486f469b9d03ba2f68890f2ab7c74
Emilia Brzoskowska <emilia.brzoskowska@gmail.com>
2021-08-03 07:24:08 +0100

    server & connecion with mongo db

---
+++ src/db/connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connection = async () => {
+    try 
+    {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+      });
+      console.log('succesfully connected!')
+    } 
+    catch (error)
+    {
+      console.log(error)
+    }
+}
+
+connection();
---
+++ src/server.js
@@ -0,0 +1,14 @@
+require('./db/connection');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const userRouter = require('./users/users.routes');
+
+app.use(express.json());
+app.use(cors());
+app.use(userRouter);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+})
